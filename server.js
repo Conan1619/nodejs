aacde5ea10585880db80cee7b4c84920579ddc67
conankudo16 <danteconankun@gmail.com>
2018-12-31 10:51:52 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const hbs=require('hbs');
+
+
+var app=express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+ '/public'));
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>BROOOO!!!</h1>');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs')
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errormsg:'fuck off broo',
+        errorcode: 69
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('server is up maan')
+});
